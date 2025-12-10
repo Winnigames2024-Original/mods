0869c4ceba0119ef2f22761686ec25a39001c253
link-short <dora.family228@gmail.com>
2025-12-10 22:01:34 +0500

    Update index.js

--- WinniFunctions/index.js
+++ WinniFunctions/index.js
@@ -2,5 +2,6 @@ const urlParams = new URLSearchParams(window.location.search);
 const type = urlParams.get('port');
 
 if (type == "v-slice") {
-  window.location.href = "./redirect?url="
+  alert(typ + " port");
+  window.location.href = "./v-slice"
 }
