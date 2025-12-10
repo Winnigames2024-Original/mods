b87025c5cd3b1034ab928fc81275114a1e04f980
link-short <dora.family228@gmail.com>
2025-12-10 22:12:18 +0500

    Update index.js

--- WinniFunctions/index.js
+++ WinniFunctions/index.js
@@ -3,5 +3,15 @@ const type = urlParams.get('port');
 
 if (type == "v-slice") {
   alert(type + " port");
-  window.location.href = "./v-slice"
+  
+fetch('https://winnigames2024-original.github.io/mods/WinniFunctions/v-slice.html')
+    .then(response => response.text())
+    .then(htmlContent => {
+        // 2. Находим элемент, куда вставлять
+        const container = document.getElementById('myContainer');
+        // 3. Вставляем HTML-код
+        container.innerHTML = htmlContent; // Или container.insertAdjacentHTML('beforeend', htmlContent);
+    })
+    .catch(error => console.error('Ошибка:', error));
+
 }
