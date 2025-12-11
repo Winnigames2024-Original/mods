551fd6e59b52fa62f9fed2400f84685b5e8287b7
link-short <dora.family228@gmail.com>
2025-12-11 13:52:06 +0500

    Update index.js

--- WinniFunctions/index.js
+++ WinniFunctions/index.js
@@ -10,7 +10,7 @@ fetch('https://winnigames2024-original.github.io/mods/WinniFunctions/v-slice.htm
         // 2. Находим элемент, куда вставлять
         const container = document.getElementById('myContainer');
         // 3. Вставляем HTML-код
-        container.innerHTML = htmlContent; // Или container.insertAdjacentHTML('beforeend', htmlContent);
+        container.insertAdjacentHTML('beforebegin = htmlContent; // Или container.insertAdjacentHTML('beforeend', htmlContent);
     })
     .catch(error => console.error('Ошибка:', error));
 
