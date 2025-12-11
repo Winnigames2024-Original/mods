88b4ffbdc186bf5c00c5b8073bf79725888a7c60
link-short <dora.family228@gmail.com>
2025-12-11 20:39:25 +0500

    Update index.js

--- download/index.js
+++ download/index.js
@@ -1,5 +1,4 @@
 const urlParams = new URLSearchParams(window.location.search);
-const storageUrl = urlParams.get('storage');
 const storagepath = urlParams.get('storagePath');
 
 const full_dataurl = "https://filecache36.gamebanana.com" + storagepath;
