85f50be4de3b67328470b6caba93d049118a691d
link-short <dora.family228@gmail.com>
2025-12-11 21:57:39 +0500

    Update index.js

--- download/index.js
+++ download/index.js
@@ -2,5 +2,5 @@ const urlParams = new URLSearchParams(window.location.search);
 const storage = urlParams.get('storage');
 const storagepath = urlParams.get('storagePath');
 
-const full_dataurl = storage + storagepath;
+const full_dataurl = "https://" + storage + storagepath;
 window.location.href = full_dataurl;
