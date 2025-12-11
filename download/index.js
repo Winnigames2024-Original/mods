93975ee86a960da96d3a9ce6ec638043dd059121
link-short <dora.family228@gmail.com>
2025-12-11 20:36:00 +0500

    Update index.js

--- download/index.js
+++ download/index.js
@@ -2,19 +2,6 @@ const currentWindowParams = new URLSearchParams(window.location.search);
 const storageUrl = currentWindowParams.get('storage');
 
 if (storageUrl) {
-  if (storageUrl == "gamebanana_storage36") {
-    const storageUrlPath = currentWindowParams.get('storagePath');
-    if (storageUrlPath) {
-      const full_dataUrl = "https://filecache36.gamebanana.com" + storageUrlPath;
-      downloadURL(full_dataUrl);
-    }
-  }
+ const full_dataUrl = "https://filecache36.gamebanana.com" + storageUrlPath;
+  window.location.href = full_dataUrl;
 }
-
-function downloadURL(urlFile) {
-  const element = document.getElementById("downloadFile");
- 
-  element.href = urlFile;
-
-}
-
